01d2269ccfdd0ab8658d7ef2aeb30517817ccc09
Bram Eikelboom <bram.eikelboom@profects.com>
2025-07-03 17:29:10 +0200

    Restyle with minimal changes

--- apps/login/src/app/(login)/layout.tsx
+++ apps/login/src/app/(login)/layout.tsx
@@ -6,11 +6,8 @@ import { Logo } from "@/components/logo";
 import { Skeleton } from "@/components/skeleton";
 import { Theme } from "@/components/theme";
 import { ThemeProvider } from "@/components/theme-provider";
-import { getServiceUrlFromHeaders } from "@/lib/service-url";
-import { getBrandingSettings } from "@/lib/zitadel";
 import { Analytics } from "@vercel/analytics/react";
 import { Lato } from "next/font/google";
-import { headers } from "next/headers";
 import { ReactNode, Suspense } from "react";
 
 const lato = Lato({
@@ -23,14 +20,6 @@ export default async function RootLayout({
 }: {
   children: ReactNode;
 }) {
-  const _headers = await headers();
-  const { serviceUrl } = getServiceUrlFromHeaders(_headers);
-
-  const branding = await getBrandingSettings({
-    serviceUrl,
-    organization: undefined,
-  });
-
   return (
     <html className={`${lato.className}`} suppressHydrationWarning>
       <head />
@@ -54,28 +43,33 @@ export default async function RootLayout({
           >
             <LanguageProvider>
               <div
-                className={`relative min-h-screen bg-background-light-600 dark:bg-background-dark-600 flex flex-col justify-center`}
+                className={`relative min-h-screen  flex  gap-8 justify-center`}
               >
-                <div
-                  className="px-8"
-                  style={{
-                    paddingTop: "2rem",
-                    paddingBottom: "2rem",
-                    height: "72px",
-                  }}
-                >
+                <div className="relative ml-4  py-8 w-8/12 flex-col">
                   <Logo
-                    lightSrc={branding?.lightTheme?.logoUrl}
-                    darkSrc={branding?.darkTheme?.logoUrl}
+                    lightSrc="/logo/thanqs-logo.svg"
+                    darkSrc="/logo/thanqs-logo.svg"
                     height={150}
                     width={128}
                   />
+                  <div className="max-w-[440px] justify-center mx-auto">
+                    {children}
+                    <div className="flex flex-row justify-end py-4 items-center space-x-4">
+                      <LanguageSwitcher />
+                      <Theme />
+                    </div>
+                  </div>
                 </div>
-                <div className="relative mx-auto max-w-[440px] py-8 w-full ">
-                  {children}
-                  <div className="flex flex-row justify-end py-4 items-center space-x-4">
-                    <LanguageSwitcher />
-                    <Theme />
+                <div className="relative py-4 w-4/12 flex-col max-h-screen">
+                  <div
+                    style={{
+                      backgroundColor: "hsl(271,54%,92%)",
+                      height: "100%",
+                      width: "100%",
+                    }}
+                    className="relative m-8 hidden  w-full max-w-[566px] shrink-0 overflow-hidden rounded-[20px]  lg:block"
+                  >
+                    <GiftsAndPresentsIllustration />
                   </div>
                 </div>
               </div>
@@ -87,3 +81,26 @@ export default async function RootLayout({
     </html>
   );
 }
+
+const GiftsAndPresentsIllustration = () => (
+  <>
+    <img
+      src="/icons/ribbon.svg"
+      width={403}
+      height={403}
+      className="absolute -top-20 -right-28 -rotate-12"
+    />
+    <img
+      src="/icons/flowers.svg"
+      width={442}
+      height={442}
+      className="absolute top-[20%] -left-40 rotate-45"
+    />
+    <img
+      src="/icons/present.svg"
+      width={483}
+      height={483}
+      className="absolute -right-16 -bottom-32 -rotate-[19deg]"
+    />
+  </>
+);
